5c8f746cc840e5da8d65d072b6baf3aa42ac100a
viveknigam3003 <viveknigam.nigam3@gmail.com>
2018-12-13 22:40:48 +0530

    "First Draft"

--- hadoopsetup.py
+++ hadoopsetup.py
@@ -5,27 +5,34 @@ import authenticate
 print("\t\t\tWelcome to Hadoop Setup Tool")
 print("\t\t\t----------------------------")
 
-print("""
-	First we need to set Authentication
-
-	Authentication Setup Started...
-	""")
-
-authenticate.checkIP()
-print("\nDo You wish to Continue [y/n]?")
-ch = input()
-
-if ch == 'y':
-	authenticate.getssh()
-else:
-	exit()
-
-os.system('tput setaf 2')
-print("Authentication is complete!")
-os.system('tput setaf 7')
-
 print("Enter IP of the machine to set up: ", end=' ')
 thisip = input()
+localip = subprocess.getoutput("ifconfig enp0s3 | grep -w inet | awk '{ print $2}'")
+if thisip != localip:
+	print("The current machine IP is: {}".format(localip))
+	print("Do you wish to setup Hadoop remotely on {} ? [y/n]: ".format(thisip), end=' ')
+	rem = input()
+	if rem == "n":
+		print("\nContinuing with current machine...")
+	if rem == "y":
+		print("Setting up {} remotely.".format(thisip))
+		print("""
+				\nFirst we need to set Authentication
+				Authentication Setup Started...
+			""")
+
+		authenticate.checkIP()
+		print("\nDo You wish to Continue [y/n]?")
+		ch = input()
+
+		if ch == 'y':
+			authenticate.getssh()
+		else:
+			exit()
+
+		os.system('tput setaf 2')
+		print("Authentication is complete!")
+		os.system('tput setaf 7')
 
 print("""
 		This Machine will be used as?
